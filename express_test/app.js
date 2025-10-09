157475ff15d40674f4f281e18e6146077402bf95
Mitchell-Gerwin <m-gerwin@onu.edu>
2025-10-09 03:24:30 -0400

    Added home page

--- express_test/app.js
+++ express_test/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var cardsRouter = require('./routes/cards');
+var homeRouter = require('./routes/home');
 
 
 var app = express();
@@ -21,6 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', homeRouter )
 app.use('/', indexRouter);
 app.use('/', cardsRouter);
 app.use('/users', usersRouter);
--- express_test/routes/cards.js
+++ express_test/routes/cards.js
@@ -6,7 +6,7 @@ router.get('/cards-page2', function(req, res, next) {
 });
 
 router.get('/cards-page3', function(req, res, next) {
-  res.render('cards-page3', { card: "images/Niv-Mizzet, Parun (Doc-Gargac, Parun).png" });
+  res.render('cards-page3', { card: "images/Aragorn, the Uniter (Dr. Estell, the UI_UXer).png" });
 });
 
 
