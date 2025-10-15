09fcdf313ead58f66bdcde9fa1e7dab2466b18fb
M3nt4lG3n <143116925+M3nt4lG3n@users.noreply.github.com>
2025-10-15 11:17:46 -0400

    Oops all merge conflict resolutions

--- express_test/app.js
+++ express_test/app.js
@@ -7,11 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var cardsRouter = require('./routes/cards');
-<<<<<<< HEAD
 var fullRouter = require('./routes/full_view');
-=======
 var homeRouter = require('./routes/home');
->>>>>>> b6d14ba41caca03019fb6e3b1ba843b54856b2c6
 
 
 var app = express();
