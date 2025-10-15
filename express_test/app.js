43f6743381ebf51308d7b937f7c5b55f55d02fbd
Mitchell-Gerwin <m-gerwin@onu.edu>
2025-10-15 12:32:20 -0400

    Putting Pagination all onto Index -- Also Partial Checklist View

--- express_test/app.js
+++ express_test/app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', homeRouter )
 app.use('/index', indexRouter);
-app.use('/', cardsRouter);
+app.use('/cards', cardsRouter);
 app.use('/users', usersRouter);
 
 
