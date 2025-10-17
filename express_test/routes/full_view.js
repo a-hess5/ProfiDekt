3eb74f583d91a6095372b7dbb3a1807335d45ba3
M3nt4lG3n <143116925+M3nt4lG3n@users.noreply.github.com>
2025-10-17 14:28:51 -0400

    Finished putting game icons in oracle text

--- express_test/routes/full_view.js
+++ express_test/routes/full_view.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next){
         altName: "Niv-Mizzet, Parun",
         manaCost: '{UUURRR}',
         cardType: "Legendary Creature - Dragon Wizard",
-        oracleText: ["This spell can't be countered.", "Flying", "Whenever you draw a card, Doc-Gargac, Parun deals 1 damage to any target.", "Whenever a player casts an instant or sorcery spell, you draw a card.", "Test text {R}"],
+        oracleText: ["This spell can't be countered.", "Flying", "Whenever you draw a card, Doc-Gargac, Parun deals 1 damage to any target.", "Whenever a player casts an instant or sorcery spell, you draw a card.", "Test text {R}, {U}, Hit em again {RU}"],
         flavorText: "As a capstone professor, he is always working on a new project",
         power: 5,
         toughness: 5,
